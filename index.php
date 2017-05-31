4aec3ddd8abba029cac57e995d351a40dc0bff04
nour <challitanour@gmail.com>
2017-05-31 15:34:26 +0300

    test

--- index.php
+++ index.php
@@ -12,33 +12,30 @@
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
+ <link rel="stylesheet" href="index.css">
 <style>
-h1{
-	color:RED;
-	background-color:rgba(255,255,255,0.4);
-}
+
 </style>
 </head>
-<body background="background.jpg">
+<body>
 	<br>
 	<div style='text-align:center'>
 	<h1> WELCOME </h1>
 	<h1> TO </h1>
 	<h1> ABSENCE TRACKER</h1><br><br><b><br>
 	</div>
-	
-	
 	<form name='userLoginForm' method='post' action='userLogIn.php'> 
-		<div class="container"  style='width:30%;height:60%;text-align:center;border-style:solid;background-color:white' >
+		<div class="container">
 		<h3  style='text-align:center'>Log in</h3><br>
 		<label><b>Username</b></label>
 		<input type="text" placeholder="Enter Username" name="uname" required><br><br>
 
 		<label><b>Password</b></label>
 		<input type="password" placeholder="Enter Password" name="psw" required><br><br>
-        
-		<button type="submit">Login</button><br><br>
+        <a><i>Forgot Password?</i></a><br><br>
+		</div><br><br>
+		<div style='text-align: center;display: inline-block;'>
+		<button type="submit">SUBMIT</button><br><br>
 		</div>
 	</form><br>
 
